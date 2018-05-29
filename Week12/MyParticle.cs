742e30bcedb4d5bd8bdc05d868e4378a1756d590
wanggh8 <37770619+wanggh8@users.noreply.github.com>
2018-05-29 22:01:35 +0800

    Add files via upload

---
+++ Week12/MyParticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyParticle : MonoBehaviour {
+
+	// Use this for initialization
+	ParticleSystem exhaust;
+	void Start () {
+		exhaust = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
